733e1aa355e9007c8876e768d5614323ea324208
Seb-Leb <seb@leb.cat>
2020-02-24 13:39:31 -0500

    first commit

---
+++ modules/data_manager.py
@@ -0,0 +1,21 @@
+import pyfaidx
+import numpy as np
+import os
+
+class DataManager:
+    def __init__(self, ppi_data_path, sequence_data_path, aa_data_path):
+        self.ppi_data_path = ppi_data_path
+        self.sequence_data_path = sequence_data_path
+        self.aa_data_path = aa_data_path
+
+    def get_prot_representation(self, prot_seq):
+        return prot_rep
+
+    def parse_fasta(self, fasta):
+        return seq_dict
+
+    def parse_string(self, string):
+        """
+        net : list of tuples,
+        """
+        return net
